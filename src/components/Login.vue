4c7a7808bd1192f0bcb1e1479b22539a5c2cea96
g0tie <gaultierbegue@outlook.com>
2021-08-26 09:12:30 +0400

    première intégration front login

---
+++ src/components/Login.vue
@@ -0,0 +1,122 @@
+<template>
+
+
+    <div class="card">
+        <div class="error" v-if="error">
+            Mot de passe ou Login incorrect
+        </div> 
+
+        <h1 class="title">Se connecter</h1>
+        <form>
+            <input 
+                type="text" 
+                name="login" 
+                v-model="login" 
+                placeholder="Login"
+            />
+
+            <input 
+                type="password"     
+                name="password" 
+                v-model="password" 
+                placeholder="Mot de passe"
+            />
+
+            <button type="submit" class="submit-btn" @click="tryToLogin">Se connecter</button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data : function () {
+        return {
+            login: "",
+            password: "",
+            error: false
+        }
+    },
+    methods: {
+        // A COMPLETER QUAND PARTIE API INTEGRE
+        tryToLogin: async function (e) {
+            e.preventDefault();
+
+            let res = await axios.post(
+            // `${process.env.VUE_APP_URL}/${}/`
+            )
+
+            if (res.status != 200) {
+                this.error = true;
+            } else {
+                this.error = false;
+            }
+        },
+    }
+}
+</script>
+
+<style>
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+.card {
+    padding: 40px;
+    box-shadow: 5px 5px 5px rgb(0,0,0,0.1);
+    width:40vw;
+    height:40vh;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+form input {
+    margin: 10px 0;
+    font-size: 15px;
+    min-width: 300px;
+    min-height: 30px;
+    border:none;
+    border: solid grey 2px;
+    border-radius: 0;
+}
+
+
+
+.title {
+    margin:0 0 40px 0;
+    font-size: xxx-large;
+}
+
+.submit-btn {
+    font-size: 15px;
+    padding:10px;
+    margin-top:15px;
+    background: rgb(155, 238, 155);
+    outline:none;
+    border:none;
+    cursor:pointer
+}
+
+
+@media screen and (max-width: 720px) {
+    .card  {
+        width: 100%;
+        height: 100%;
+        box-shadow: none;
+    }
+
+    form input {
+        width: 100%;
+    }
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Planning from './components/Planning.vue'
 import Home from './components/Home.vue'
+import Login from './components/Login.vue'
 
 const routes = [
     {path : '*',component: Home},
-    {path : '/planning/:id', component : Planning}
+    {path : '/planning/:id', component : Planning},
+    {path: '/', component: Login}
 ];
 
 export default routes;
